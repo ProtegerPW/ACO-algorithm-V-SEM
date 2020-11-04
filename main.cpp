faf152605ed68fe997c8bdf060191d7af0d98432
ProtegerPW <kobolte@gmail.com>
2020-11-04 21:58:03 +0100

    sec commit 	modified:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//TODO STEP 1 - read input file & create matrix
+//TODO STEP 2 -
+
+//TODO matrix of cities & their connections - GRAPH
+//TODO matrix of visibility (distance)
+//TODO matrix of pheromone value
+
+//TODO matrix to store probability
+
+//TODO matrix of deltapheromone (added at the end)
+
+//TODO matrix to store path of ant(s)
